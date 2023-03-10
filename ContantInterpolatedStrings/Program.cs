ff136c0e88591a097b1e55fcfad8269c701fad7b
DeployChef <dc.titovvlad@gmail.com>
2023-03-10 20:33:37 +0300

    2 Feature

---
+++ ContantInterpolatedStrings/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConstantInterpolatedStrings;
+
+class Program
+{
+    const string BaseUrl = "https://learn.microsoft.com/ru-ru/dotnet/";
+
+    const string CSharpUrl = BaseUrl + "/csharp";
+
+    // C#10
+    const string CSharpUrlInterpolate = $"{BaseUrl}/csharp";
+}
