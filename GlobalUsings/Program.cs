8fc5f783b274785c946d818a833f197493c6f119
DeployChef <dc.titovvlad@gmail.com>
2023-03-10 20:34:35 +0300

    7 Feature

---
+++ GlobalUsings/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace GlobalUsings;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var names = new { Hello = "Hello", World = "World" };
+        var serialized = JsonSerializer.Serialize(names);
+
+        Console.WriteLine(serialized);
+    }
+}
+
+
---
+++ RecordSealToString/RecordSealToString.cs
@@ -0,0 +1,20 @@
+﻿namespace RecordSealToString
+{
+    public record Parent(string FirstName, string LastName)
+    {
+        public sealed override string ToString()
+        {
+            return $"Hi my name is: {FirstName} {LastName}";
+        }
+    }
+
+    public record Child(string FirstName, string LastName, string Phone)
+        : Parent(FirstName, LastName)
+    {
+        //Невозможно оверрайдить при sealed
+        //public  override string ToString()
+        //{
+        //    return $"Hi my name is: {FirstName} {LastName}. And my Phone is: {Phone}";
+        //}
+    }
+}
