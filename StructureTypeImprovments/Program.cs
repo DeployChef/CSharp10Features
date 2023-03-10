f004d1e9c93dd39a6840300b272ae258375a773d
DeployChef <dc.titovvlad@gmail.com>
2023-03-10 20:34:58 +0300

    10 Feature

---
+++ StructureTypeImprovments/Program.cs
@@ -0,0 +1,8 @@
+﻿using StructureTypeImprovements;
+
+var rectangle = new Rectangle(100, 100);
+
+var rectangleDefault = new Rectangle();
+
+//copy with 
+var rectangleCopy = rectangle with { Width = 50 };
---
+++ StructureTypeImprovments/Rectangle.cs
@@ -0,0 +1,21 @@
+namespace StructureTypeImprovements
+{
+    public struct Rectangle
+    {
+        public Rectangle()
+        {
+            Height = 0;
+            Width = 0;
+        }
+
+        public Rectangle(int height, int width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public int Height { get; init; }
+
+        public int Width { get; init; }
+    }
+}
