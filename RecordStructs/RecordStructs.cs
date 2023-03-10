9fd308d2af336609658663a72ac30d85f4222449
DeployChef <dc.titovvlad@gmail.com>
2023-03-10 20:39:54 +0300

    9 Feature

--- RecordStructs/RecordStructs.cs
+++ RecordStructs/RecordStructs.cs
@@ -7,7 +7,7 @@ public class Example
 {
     public void Main()
     {
-        var person = new Person("Person", "P");
-        var client = new Client("Client", "C");
+        var person = new Person("Person", "Person");
+        var client = new Client("Client", "Client");
     }
 }
