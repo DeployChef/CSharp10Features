fd10d743218c30b5a1dd96f69feb27cc08205c09
DeployChef <dc.titovvlad@gmail.com>
2023-03-10 20:34:47 +0300

    8 Feature

---
+++ RecordStructs/RecordStructs.cs
@@ -0,0 +1,13 @@
+﻿namespace RecordStructs;
+
+public record class Person(string FirstName, string LastName);
+public readonly record struct Client(string FirstName, string LastName);
+
+public class Example
+{
+    public void Main()
+    {
+        var person = new Person("Person", "P");
+        var client = new Client("Client", "C");
+    }
+}
